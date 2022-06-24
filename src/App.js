ba12fde18ae48b5789cbd648a051d8c54ad01962
Daniel Sanchez <danxschz@gmail.com>
2022-06-24 01:15:24 -0400

    Add resume state and function to handle input changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
 import EducationForm from "./components/EducationForm";
 import ExperienceForm from "./components/ExperienceForm";
 import ContactForm from "./components/ContactForm";
+import { Component } from "react";
+import Resume from "./components/Resume";
 
-function App() {
-  return (
-    <div className="App">
-      <ContactForm/>
-      <ExperienceForm/>
-      <EducationForm/>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      resume: {
+        fullName: '',
+        email: '',
+        phone: '',
+        linkedIn: '',
+        personalWebsite: '',
+        country: '',
+        state: '',
+        city: ''
+      }
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      resume: {
+        ...this.state.resume,
+        [e.target.id]: e.target.value
+      },
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <ContactForm handleChange={this.handleChange}/>
+        <ExperienceForm/>
+        <EducationForm/>
+        <Resume resume={this.state.resume}/>
+      </div>
+    );
+  }
 }
 
 export default App;
